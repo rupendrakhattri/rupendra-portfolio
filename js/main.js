a2a8c7c40d65cec8d95360aaeb5612a774b936bb
Rupendra Khattri <105481454+rupendrakhattri@users.noreply.github.com>
2026-07-10 07:07:21 +0545

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -113,3 +113,12 @@ if(year){
     year.innerHTML = new Date().getFullYear();
 
 }
+
+const menu = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav-links");
+
+menu.addEventListener("click", () => {
+
+    nav.classList.toggle("active");
+
+});
